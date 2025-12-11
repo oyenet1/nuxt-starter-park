1684be1ceffdb3eb57d82ecb3251c1d9b58c10f7
oyenet1 <bowofadeoyerinde@gmail.com>
2025-12-11 23:30:09 +0100

    still working here

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/scripts', '@nuxt/ui']
+  modules: [
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+    '@nuxtjs/supabase'
+  ]
 })
