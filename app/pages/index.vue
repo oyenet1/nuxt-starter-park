b45680e306b90102515ca4af1ce08c08a224dd83
oyenet1 <bowofadeoyerinde@gmail.com>
2025-12-13 23:55:05 +0100

    setup of app done

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    
+    <h1 class="text-5xl text-primary">Homepage</h1>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
   modules: ["@nuxt/image", "@nuxt/scripts", "@nuxt/ui", "@nuxtjs/supabase"],
   supabase: {
     redirectOptions: {
@@ -11,5 +12,9 @@ export default defineNuxtConfig({
       exclude: [],
       saveRedirectToCookie: false,
     },
+  },
+
+  ui: {
+    colorMode: false,
   },
 });
